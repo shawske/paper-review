39397394d10001fa734c51273401be42acef7a18
shawske <rjshaw@umich.edu>
2023-12-05 10:32:25 -0500

    reviewerPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import SystemAdminPage from './pages/SystemAdminPage';
 import AuthorPage from './pages/AuthorPage';
+import ReviewerPage from './pages/ReviewerPage';
 import './NavBar.css';
 
 const App: React.FC = () => {
@@ -12,6 +13,7 @@ const App: React.FC = () => {
         <Route path="/" element={<LoginPage />} />
         <Route path="/admin" element={<SystemAdminPage />} />
         <Route path="/author" element={<AuthorPage />} />
+        <Route path="/reviewer" element={<ReviewerPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/ReviewerPage.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+const ReviewerPage: React.FC = () => {
+  return (
+    <div>
+      <NavBar />
+      <div style={{ textAlign: 'center', maxWidth: '500px', margin: 'auto' }}>
+        <h1>Reviewer Page!</h1>
+        <p>Enter your username and password to access your account.</p>
+
+        <form>
+          <div style={{ marginBottom: '1em' }}>
+            <input type="text" placeholder="Username" style={{ width: '100%', padding: '10px' }} />
+          </div>
+          <div style={{ marginBottom: '1em' }}>
+            <input type="password" placeholder="Password" style={{ width: '100%', padding: '10px' }} />
+          </div>
+          <div style={{ marginBottom: '1em' }}>
+            <button 
+              type="submit" 
+              disabled 
+              style={{ width: '100%', padding: '10px', backgroundColor: 'black', color: 'white', border: 'none' }}
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
+        <div style={{ margin: '1em 0' }}>
+          <a href="#" onClick={(e) => e.preventDefault()}>Forgot your password?</a>
+        </div>
+
+        <div style={{ borderTop: '1px solid #ccc', paddingTop: '1em', margin: '1em 0' }}>
+          <span>or continue with</span>
+        </div>
+
+        <div style={{ display: 'flex', justifyContent: 'space-around', marginBottom: '1em' }}>
+          <button 
+            disabled 
+            style={{ padding: '10px', flexGrow: 1, marginRight: '5px', backgroundColor: 'black', color: 'white', border: 'none' }}
+          >
+            Google
+          </button>
+          <button 
+            disabled 
+            style={{ padding: '10px', flexGrow: 1, marginLeft: '5px', backgroundColor: 'black', color: 'white', border: 'none' }}
+          >
+            Facebook
+          </button>
+        </div>
+
+        <div>
+          Don't have an account? <a href="#" onClick={(e) => e.preventDefault()}>Register</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewerPage;
