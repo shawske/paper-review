a259a75979ec1a70a5b62dd6dd8befd45bb93e2c
shawske <rjshaw@umich.edu>
2023-12-05 10:10:59 -0500

    54

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import LoginPage from './pages/LoginPage';
+import SystemAdminPage from './pages/SystemAdminPage';
 import './NavBar.css';
 
 const App: React.FC = () => {
   return (
     <div>
       <LoginPage />
+      <SystemAdminPage />
     </div>
+    
   );
 };
 
